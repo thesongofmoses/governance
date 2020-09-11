4e2bb2d0f45110f065b81e7a2ac8024c59d6d66b
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-09-11 08:39:54 +0000

    Update confirm.sh

--- confirm.sh
+++ confirm.sh
@@ -3,9 +3,6 @@
 username=$(whoami)
 
 cd ~/tonos-cli/target/release
-./tonos-cli run 0:db99769aa440d5ae8ce33f32d02a31c3979af04852227237b8d741c035be0a66 \
-        getTransactions {} --abi SafeMultisigWallet.abi.json | awk 'FNR == 20 {print $2}' | tr -d '"\,' > ~/tonos-cli/target/release/gov.confirm.txid
-
 ./tonos-cli call 0:db99769aa440d5ae8ce33f32d02a31c3979af04852227237b8d741c035be0a66 \
         confirmTransaction "{"\"transactionId"\":"\"${1}"\"}" \
         --abi SafeMultisigWallet.abi.json --sign ~/tonos-cli/target/release/"$username.keys.json"
