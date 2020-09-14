95d3e229419a866547228e011ba0270adc526259
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-09-14 07:21:21 +0000

    Update create.sh

--- create.sh
+++ create.sh
@@ -4,3 +4,5 @@ username=$(whoami)
 
 cd ~/tonos-cli/target/release
 ./tonos-cli getkeypair $username.keys.json "${1}"
+./tonos-cli genaddr SafeMultisigWallet.tvc SafeMultisigWallet.abi.json --setkey $username.keys.json --wc 0
+./tonos-cli genaddr SafeMultisigWallet.tvc SafeMultisigWallet.abi.json --setkey $username.keys.json --wc 0 | awk 'FNR == 9 {print $3}' > $username.addr
